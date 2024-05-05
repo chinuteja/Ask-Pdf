50cd98b43a3a3ee9813ed12b816d530d40d37e8a
chinuteja <41884987+chinuteja@users.noreply.github.com>
2024-05-05 09:49:48 +0530

    added ask pdf code

---
+++ Ask Pdf/app.py
@@ -0,0 +1,115 @@
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+# from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_cohere import CohereEmbeddings
+
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.llms import OpenAI
+from langchain_community.callbacks import get_openai_callback
+from langchain_community.llms import Cohere
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import PromptTemplate
+
+
+load_dotenv()
+
+
+
+
+def get_pdf_text(pdf_docs):
+    '''
+    extracts text from pdf files
+    '''
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+def get_text_chunks(text):
+    '''
+    splits the text extracted into chunks
+    '''
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+
+    '''
+    gets the embeddings from FAISS and stores them in local
+    '''
+    embeddings = CohereEmbeddings()
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    '''
+    A prompt template and creates a chain
+    '''
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, 
+    make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = Cohere(temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+def user_input(user_question):
+
+    '''
+    Takes the user input and generates the response
+    '''
+
+    embeddings = CohereEmbeddings()
+
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    st.write("Reply: ", response["output_text"])
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                st.write("got raw text here")
+                text_chunks = get_text_chunks(raw_text)
+                print("text chunks")
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
